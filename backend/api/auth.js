3b70c98aeb0795be9159c72155eab94988193eba
GLZX4 <2218541@chester.ac.uk>
2025-02-03 13:12:22 +0000

    adjusted again

--- backend/api/auth.js
+++ backend/api/auth.js
@@ -83,7 +83,7 @@ module.exports = (pool) => {
             const user = result.rows[0];
 
             // Verify password
-            const passwordMatch = await bcrypt.compare(password, user.userpassword);
+            const passwordMatch = await bcrypt.compare(password, user.password);
             if (!passwordMatch) {
                 return res.status(401).json({ message: 'Invalid email or password.' });
             }
