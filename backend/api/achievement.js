9effd35ddbaecd1d6a0e431513ba357f8f4f74b2
George <2218541@chester.ac.uk>
2025-04-28 18:21:10 +0100

    removed duplicate verifyToken decleration

--- backend/api/achievement.js
+++ backend/api/achievement.js
@@ -1,7 +1,6 @@
 const express = require('express');
 require('dotenv').config();
 const verifyToken = require('../middleware/authMiddleware');
-const verifyToken = require('../middleware/authMiddleware');
 const { updateAchievementProgress, fetchStudentAchievements } = require('../services/achievementService');
 
 module.exports = (pool) => {
